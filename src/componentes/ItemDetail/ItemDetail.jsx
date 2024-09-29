79ded411c6e92f5189339552d243c2a9e9f141d0
ivan.cinho <pabloivan84@gmail.com>
2024-09-29 17:58:02 -0300

    29/9

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,17 @@
 import "../ItemDetail/ItemDetail.css"
+import Contador from "../Contador/Contador.jsx"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
-const ItemDetail = ({id, nombre, precio, img, idCat}) => {
+const ItemDetail = ({id, nombre, precio, img, idCat, stock}) => {
+
+    const [agregarCantidad, setAgregarCantidad] = useState(0)
+
+    const manejadorCantidad = (cantidad) => {
+        setAgregarCantidad(cantidad)
+        console.log("productos agregados:" + cantidad)
+    }
+
     return (
         <div className="cardDetail">
             <h3>Nombre: {nombre}</h3>
@@ -9,6 +20,11 @@ const ItemDetail = ({id, nombre, precio, img, idCat}) => {
             <p>Categoria: {idCat}</p>
             <img src={img} alt={nombre} />
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, tempora.</p>
+
+        {
+            agregarCantidad > 0 ? (<Link to="/cart">Terminar compra</Link>) : (<Contador inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/> )
+        }
+
         </div>
     )
 }
--- src/componentes/asyncmock.js
+++ src/componentes/asyncmock.js
@@ -1,13 +1,13 @@
 
 const misProductos = [
-    {id: "1" , nombre: "Item 1" , precio: 200, img: "https://picsum.photos/300?random=1", idCat: "A"},
-    {id: "2" , nombre: "Item 2", precio: 300, img: "https://picsum.photos/300?random=5", idCat: "A"},
-    {id: "3" , nombre: "Item 3", precio: 400, img: "https://picsum.photos/300?random=8", idCat: "B"},
-    {id: "4" , nombre: "Item 4", precio: 500, img: "https://picsum.photos/300?random=54", idCat: "B"},
-    {id: "5" , nombre: "Item 5", precio: 600, img: "https://picsum.photos/300?random=10", idCat: "A"},
-    {id: "6" , nombre: "Item 6", precio: 700, img: "https://picsum.photos/300?random=21", idCat: "B"},
-    {id: "7" , nombre: "Item 7", precio: 800, img: "https://picsum.photos/300?random=14", idCat: "A"},
-    {id: "8" , nombre: "Item 8", precio: 900, img: "https://picsum.photos/300?random=16", idCat: "B"}
+    {id: "1" , nombre: "Item 1" , stock: 10, precio: 200, img: "https://picsum.photos/300?random=1", idCat: "A"},
+    {id: "2" , nombre: "Item 2", stock: 20, precio: 300, img: "https://picsum.photos/300?random=5", idCat: "A"},
+    {id: "3" , nombre: "Item 3", stock: 30, precio: 400, img: "https://picsum.photos/300?random=8", idCat: "B"},
+    {id: "4" , nombre: "Item 4", stock: 40, precio: 500, img: "https://picsum.photos/300?random=54", idCat: "B"},
+    {id: "5" , nombre: "Item 5", stock: 50, precio: 600, img: "https://picsum.photos/300?random=10", idCat: "A"},
+    {id: "6" , nombre: "Item 6", stock: 60, precio: 700, img: "https://picsum.photos/300?random=21", idCat: "B"},
+    {id: "7" , nombre: "Item 7", stock: 70, precio: 800, img: "https://picsum.photos/300?random=14", idCat: "A"},
+    {id: "8" , nombre: "Item 8", stock: 80, precio: 900, img: "https://picsum.photos/300?random=16", idCat: "B"}
 ]
 
 export const getProductos = () => {
