b547140cd5cd24648ce49c21d882b6ee7eb803ee
ivan.cinho <pabloivan84@gmail.com>
2024-09-30 14:34:19 -0300

    30-9

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailCon
 import ItemListCotainer from "./componentes/ItemListContainer/ItemListContainer";
 import NavBar from "./componentes/NavBar/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CarritoProvider } from "./Context/CarritoContext";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+<CarritoProvider>
       <NavBar/>
         <Routes>
           <Route path="/" element={<ItemListCotainer/>}/>
           <Route path="/categoria/:idCategoria" element={<ItemListCotainer/>}/>
           <Route path="/item/:idItem" element={<ItemDetailContainer/>}/>
         </Routes>
+        </CarritoProvider>
       </BrowserRouter>  
 
     </> 
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -2,14 +2,21 @@ import "../ItemDetail/ItemDetail.css"
 import Contador from "../Contador/Contador.jsx"
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { carritoContext } from "../../Context/CarritoContext.jsx"
+import { useContext } from "react"
 
 const ItemDetail = ({id, nombre, precio, img, idCat, stock}) => {
 
     const [agregarCantidad, setAgregarCantidad] = useState(0)
 
+    const {agregarAlCarrito} = useContext(carritoContext)
+
     const manejadorCantidad = (cantidad) => {
         setAgregarCantidad(cantidad)
         console.log("productos agregados:" + cantidad)
+
+        const item = {id, nombre, precio}
+        agregarAlCarrito(item, cantidad)
     }
 
     return (
